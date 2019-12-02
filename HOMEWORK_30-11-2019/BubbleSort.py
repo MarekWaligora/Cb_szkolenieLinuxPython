3aaca139f8fb57f0b73dafedd2e958a84222620a
MarekWaligora <mwaligor69@gmail.com>
2019-12-02 15:57:54 +0100

    Modified checks in while, remowed i<1 in if condition

--- HOMEWORK_30-11-2019/BubbleSort.py
+++ HOMEWORK_30-11-2019/BubbleSort.py
@@ -2,10 +2,10 @@ listToSort=[8,2,7,6,3,8,1,7]
 isSwapped=True
 print("Before sort: " + str(listToSort))
 listRange=len(listToSort)
-while isSwapped==True:
+while isSwapped:
     isSwapped=False
     for i in range(1,listRange):
-        if listToSort[i]<listToSort[i-1] and i>0:
+        if listToSort[i]<listToSort[i-1] :
             temp=listToSort[i-1]
             listToSort[i-1]=listToSort[i]
             listToSort[i]=temp
